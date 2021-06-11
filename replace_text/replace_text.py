1b7458a88d8139b2ecd494df286a719f8013633d
jakeogh <github.com@v6y.net>
2021-06-10 21:21:14 -0700

    auto-commit

--- replace_text/replace_text.py
+++ replace_text/replace_text.py
@@ -457,7 +457,7 @@ def cli(ctx,
                     ic(match_count, input_fh)
                 if match_count > 0:
                     sys.stdout.buffer.write(str(match_count).encode('utf8'))
-                    sys.stdout.buffer.write(input_fh)
+                    sys.stdout.buffer.write(str(input_fh).encode('utf8'))
                     sys.stdout.buffer.write(end)
                     #print(match_count, input_fh, end=end)
 
