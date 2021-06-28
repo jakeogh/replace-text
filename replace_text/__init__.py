9e77868513a2397035be1a0df9283e7635e5d3b7
jakeogh <github.com@v6y.net>
2021-06-27 19:03:48 -0700

    auto-commit

--- replace_text/__init__.py
+++ replace_text/__init__.py
@@ -1,3 +1,4 @@
+#from .replace_text import replace_text
 from .replace_text import append_unique_bytes_to_file
 from .replace_text import remove_comments_from_bytes
-from .replace_text import replace_text
+from .replace_text import replace_text_in_file
