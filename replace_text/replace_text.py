04e63f9b7ea9d6b24c57811b6f56b74ec9835c45
jakeogh <github.com@v6y.net>
2025-10-15 16:43:19 -0700

    auto-commit

--- replace_text/replace_text.py
+++ replace_text/replace_text.py
@@ -3,21 +3,6 @@
 # Replace the given text in file(s) or stdin.
 """
 
-# pylint: disable=C0111     # docstrings are always outdated and wrong
-# pylint: disable=W0511     # todo is encouraged
-# pylint: disable=C0301     # line too long
-# pylint: disable=R0902     # too many instance attributes
-# pylint: disable=C0302     # too many lines in module
-# pylint: disable=C0103     # single letter var names, func name too descriptive
-# pylint: disable=R0911     # too many return statements
-# pylint: disable=R0912     # too many branches
-# pylint: disable=R0915     # too many statements
-# pylint: disable=R0913     # too many arguments
-# pylint: disable=R1702     # too many nested blocks
-# pylint: disable=R0914     # too many local variables
-# pylint: disable=R0903     # too few public methods
-# pylint: disable=E1101     # no member for base
-# pylint: disable=W0201     # attribute defined outside __init__
 from __future__ import annotations
 
 import os
@@ -28,13 +13,13 @@ import tempfile
 from pathlib import Path
 
 import click
-from asserttool import gvd
 from asserttool import ic
 from asserttool import maxone
 from clicktool import click_add_options
 from clicktool import click_global_options
 from clicktool import tvicgvd
 from eprint import eprint
+from globalverbose import gvd
 from mptool import output
 from unmp import unmp
 
@@ -208,7 +193,7 @@ def replace_text_line(
 
     # this cant handle binary files... or files with mixed newlines
     modified = False
-    with open(path, "rU") as path_fh:
+    with open(path) as path_fh:
         try:
             for line in path_fh:
                 if match_bytes in line:
